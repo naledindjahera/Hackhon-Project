10f7d00099de5960b1a0a34352e2fe119b96d4b9
Daholva Ndjahera <nnaledi1166@gmail.com>
2026-08-18 14:27:46 +0200

    add global api error handling

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,24 @@ app.get("/", (req, res) => {
 
 app.use("/api/projects", projectRoutes);
 
+
+// Handle unknown routes
+app.use((req, res) => {
+    res.status(404).json({
+        message: "Endpoint not found"
+    });
+});
+
+
+// Global error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+
+    res.status(500).json({
+        message: "Internal server error"
+    });
+});
+
 // Start server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
